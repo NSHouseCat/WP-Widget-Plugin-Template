a7b789fb4700533fff08bd614b05ad97443dbc75
Andrew Nordahl <drewdahl@gmail.com>
2018-11-27 01:08:31 -0800

    Clean up

--- wp-widget-plugin-template.php
+++ wp-widget-plugin-template.php
@@ -3,14 +3,16 @@
 Plugin Name: Widget plugin name
 Description: Widget plugin description text
 */
-/* Start Adding Functions Below this Line */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
 // Register and load the widget
 function load_widget_plugin_name() {
   register_widget( 'widget_plugin_name' );
 }
 add_action( 'widgets_init', 'load_widget_plugin_name' );
 
-// Creating the widget 
+// Create widget class 
 class widget_plugin_name extends WP_Widget {
   function __construct() {
     parent::__construct(
@@ -23,40 +25,36 @@ class widget_plugin_name extends WP_Widget {
     );
   }
 
-  // Creating widget front-end
+  // Widget front-end
   public function widget( $args, $instance ) {
     $title = apply_filters( 'widget_title', $instance['title'] );
-    // before and after widget arguments are defined by themes
+    // before and after widget arguments are defined by theme
     echo $args['before_widget'];
     if ( ! empty( $title ) )
     echo $args['before_title'] . $title . $args['after_title'];
-    echo __( $instance[ 'title' ], 'widget_plugin_name_domain' );
+    echo __( $instance['title'], 'widget_plugin_name_domain' );
     echo $args['after_widget'];
   }
         
-  // Widget Backend 
+  // Widget backend 
   public function form( $instance ) {
-    if ( isset( $instance['title'] ) ) {
-      $title = $instance['title'];
-    } else {
-      $title = __( 'New title', 'widget_plugin_name_domain' );
-    }
+    $title = (isset( $instance['title'] )) ? $instance['title'] : __( '', 'widget_plugin_name_domain' );
     // Widget admin form
     ?>
     <p>
-<!-- Widget Title Form Entry -->    
-      <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Widget Title:' ); ?></label> 
-      <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+<!-- Widget Title -->    
+      <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Widget Title:'); ?></label> 
+      <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
       <hr>
     </p>
     <?php 
   }
-  // Updating widget replacing old instances with new
+  
+  // Updating widget
   public function update( $new_instance, $old_instance ) {
     $instance = array();
     $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
     return $instance;
   }
-} // Class widget_plugin_name ends here  
-/* Stop Adding Functions Below this Line */
+}
 ?>
